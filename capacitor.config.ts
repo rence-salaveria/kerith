07b802470b66038fcf9f325d54777202a26b191b
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-08 19:28:12 +0800

    [INITIAL] / Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mad Cat',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
